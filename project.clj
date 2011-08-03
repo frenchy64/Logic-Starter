63489eb20f004782143985ae07822a8eb0b75ef8
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-08-03 16:20:06 +0800

    Updated core.logic dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject logic-introduction "1.0.0-SNAPSHOT"
   :description "Introduction to Logic Programming"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/core.logic "0.6.2"]])
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+                 [org.clojure/core.logic "0.6.3"]])
