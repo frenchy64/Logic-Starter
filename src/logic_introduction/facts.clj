45674e3a49aebf60e05d8d69425ea5493bb01922
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-09-08 23:25:16 +0800

    Fixed type sort where both types are identical

--- src/logic_introduction/facts.clj
+++ src/logic_introduction/facts.clj
@@ -11,11 +11,8 @@
 (fact derives Number Float)
 
 (fact prefer Integer Float)
-(fact prefer Number Float)
-(fact prefer Number Integer)
 
 (fact derives Object Number)
-(fact derives Object String)
 (fact derives Object String)
 (fact derives Object Character)
 
@@ -23,39 +20,33 @@
 (fact prefer Character String)
 (fact prefer Number Character)
 
-(fact prefer Object Number)
-(fact prefer Object String)
-(fact prefer Object Integer)
-(fact prefer Number Number)
 
-(def derived-ancestors 
+(def derived-ancestor
   (tabled [ancestor descendant]
     (conde
       ((derives ancestor descendant))
       ((exist [i]
-         (derived-ancestors ancestor i)
+         (derived-ancestor ancestor i)
          (derives i descendant))))))
 
 (defne appendo [x y z]
-         ([() _ y])
-         ([[?a . ?d] _ [?a . ?r]] (appendo ?d y ?r)))
+       ([() _ y])
+       ([[?a . ?d] _ [?a . ?r]] (appendo ?d y ?r)))
+
+(defn printlno [& rest]
+  (== nil (do (apply println rest)
+            (flush))))
 
 (defn partition-by-preference [xs pivot littles bigs]
-  (matcha [xs pivot littles bigs]
-          ([[?x . _] ?y [?x . _] _]
-           (prefer ?x ?y)
-           (prefer ?y ?x)
-           (project [?x ?y]
-                    (== nil (println "Ambiguous preference"))))
+  (matche [xs pivot littles bigs]
           ([[?x . ?xs] ?y [?x . ?littles] ?bigs]
-           (prefer ?x ?y)
+           (conda
+             ((== ?x pivot))
+             ((derived-ancestor ?x ?y)))
            (partition-by-preference ?xs ?y ?littles ?bigs))
           ([[?x . ?xs] ?y ?littles [?x . ?bigs]]
-           (prefer ?y ?x)
+           (derived-ancestor ?y ?x)
            (partition-by-preference ?xs ?y ?littles ?bigs))
-          ([[?x . ?xs] ?y _ _]
-           (project [?x ?y]
-                    (== (println "Exception: No preference: " ?x ?y) nil)))
           ([() pivot () ()])))
 
 (defn sort-by-preference [ts out]
