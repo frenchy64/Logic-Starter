3b3d23d020f10f705b0dcb90e7f69d3486214b4f
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-09-26 00:40:55 +0800

    More changes to talk ideas

--- src/logic_introduction/decl_model.clj
+++ src/logic_introduction/decl_model.clj
@@ -25,17 +25,124 @@
 (defn dataflow []
   (DataFlow. (atom unbound-dataflow)))
 
-;; # Logical semantics
+
+
+
+
+;; Semantics
+
+;; Two ways to look at a piece of functionality:
+;;
+;; Logical view: statement of logic
+;; Operational view: execution on a computer
+
+;; # Example with Functional Programming
+;; ## Logical semantics
+;;
+;; Return value is a appended to b
+;;
+;; ## Operational semantics
+;;
+;; If a is empty, return b, otherwise return rest of a
+;; appended to the cons of first of a and b.
+
+(defn append [a b]
+  (if (empty? a)
+    b
+    (let [[x & as] a]
+      (append as (cons x b)))))
+
+;; # Eliminate return value
+;;
+;; Formal logic is normally expressed in terms of variables. This does
+;; not map well to the notion of "returning" a value.
+;;
+;; Convert the return value to a parameter "c" ..
+;;
+;; (defn append [a b c]
+;;    ...)
+;;
+;; and we can express the Logical Semantics more correctly:
+;;
+;; c is a appended to b
+
+
+;; # Directionality
+;;
+;; In functional programming "return values" are for output
+;; and parameters are for "input"
+;;
+;; To play the role of the return value, we need an "output" parameter.
+
+
+;; # Output Parameter?
+;;
+;; The caller of the function should initialize some sort
+;; of variable and pass this to the function. After execution
+;; we should expect the variable to be changed.
+;;
+;; Variables should be "bind-once" after being initialized to unbound.
+;; 
+;; Much safer than "output" parameters with pointers in C, instead bind-once
+;; with immutable values. Similar concept.
+
+
+
+;; # Dataflow variable
+;; 
+;; For educational purposes only ..
+;;
+;; Bind-once semantics.
+;;
+;; See implementation
+
+
+;; # Shape
+;;
+;; Computation has a different shape using output parameters
+;;
+;; (let [x (dataflow)]
+;;   (append [1] [2] x)
+;;   @x)
+;; ;=> [1 2]
+;; 
+;; Feels like abstracting away assignment
+
+
+;; # Collecting results
+;;
+;; Abstract this pattern with "solve-dataflow". Implementation above.
+;;
+;; (solve-dataflow [x]
+;;   (append [1] [2] x))
+;; ;=> [1 2]
+;;
+;; Returns the value of dataflow variable x after executing body.
+
+
+;; # Patterns of input/output arguments
+;;
+;; From the logical semantics of "append", we can deduce 
+;; some interesting relationships
+;;
+;; If c is a appended to b
+;; then a should be everything in c before b
+;;
+;; If c is a appended to b
+;; then b should be everything in c after a
+
+
+;; # append-iio
+;;
+;; ## Logical semantics
 ;;
 ;; c# is a appended to b
 ;;
-;; # Operational semantics
+;; ## Operational semantics
 ;;
 ;; Sets the dataflow variable c# to a appended to b
 
-(defn append-iio
-  "Declarative model append"
-  [a b c#]
+(defn append-iio [a b c#]
   (if (empty? a)
     (set-once! c# b)
     (let [[x & as] a
@@ -78,11 +185,57 @@
 ;; Our model is deterministic (it gives just one solution).
 
 
+
 ;; Have you noticed that the logical semantics are identical for
 ;; all versions of append, yet the operational semantics are different?
 ;;
 ;; What if we could model the operational semantics in terms of the logical
 ;; semantics?
+;;
+;; This requires a paradigm change
 
-(defn appendo [x y z]
-  (matche []
+
+;; # Nondeterministic logic programming
+;;
+;; We've just seen a limited version of deterministic logic programming.
+;;
+;; Characteristics of deterministic LP
+;; - directional (it works for only one pattern of input/output arguments)
+;; - deterministic (gives just one solution)
+;;
+;; Problem: limited mapping from logical semantics to operational semantics.
+;;
+;; Nondeterministic LP:
+;; - more flexible operational semantics
+
+
+(defn appendo [a b c]
+  (matche [a b c]
+          ([[] _ _]
+           (== b c))
+          ([[?x . ?as] _ [?x . ?cs]]
+           (appendo ?as b ?cs))))
+
+;; # appendo
+;;
+;; appendo is a relation.
+;;
+;; Relations are the building blocks of nondeterministic logic programming
+;; - no distinguishing between input/output parameters (wow!)
+;; - are themselves built from other relations
+
+
+;; # run
+;;
+;; Nondeterministic version of the deterministic solve-dataflow.
+;;
+;; Returns a list results. Nondeterministic, multiple results!
+;;
+;; Specify how many results we wish to collect
+
+(run 1 [q]
+     (appendo [1] [2] q))
+;=> ([1 2])
+
+
+
