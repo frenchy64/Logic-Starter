c2ca49b934d851d03559abe9e445fd88e3b6504c
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-01-04 07:01:24 +0800

    Add query example as a goal

--- src/logic_introduction/join.clj
+++ src/logic_introduction/join.clj
@@ -24,6 +24,8 @@
 ;; There is no country in the restricted-to list for that item
 ;; The country/item pair is in the restricted-to list
 
+;; As a function
+
 (defn get-items-colors-for-country [country]
   (run* [q]
     (fresh [item-name item-color not-country]
@@ -43,3 +45,47 @@
         [(not-allowed-in country item-name)
          fail]
         [succeed]))))
+
+(get-items-colors-for-country 'US)
+;=> ([Purse Blue] [Banana Yellow] [Car Red])
+
+(get-items-colors-for-country 'UK)
+;=> ([Purse Blue])
+
+(get-items-colors-for-country 'France)
+;=> ([Banana Yellow])
+
+(get-items-colors-for-country 'Australia)
+;=> ([Purse Blue] [Banana Yellow])
+
+
+;; Or as a goal
+
+(defn items-colors-for-country [country item-name item-color]
+  (fresh [not-country]
+    (items item-name item-color)
+    (!= country not-country)
+
+    (conda
+      [(restricted-to country item-name)
+       (conda
+         [(not-allowed-in country item-name)
+          fail]
+         [succeed])]
+      [(restricted-to not-country item-name)
+       fail]
+      ;; No entry in restricted-to for item-name
+      [(not-allowed-in country item-name)
+       fail]
+      [succeed])))
+
+(run* [q]
+  (fresh [country item-name item-color]
+    (== q [country item-name item-color])
+    (conde
+      [(== 'US country)]
+      [(== 'UK country)]
+      [(== 'France country)]
+      [(== 'Australia country)])
+    (items-colors-for-country country item-name item-color)))
+;=> ([US Purse Blue] [UK Purse Blue] [US Banana Yellow] [US Car Red] [Australia Purse Blue] [Australia Banana Yellow] [France Banana Yellow])
